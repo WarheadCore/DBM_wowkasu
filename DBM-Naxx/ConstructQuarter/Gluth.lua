3847e479374e25aa8ef89eda87fb6d443e4213d9
Winfidonarleyan <dowlandtop@yandex.com>
2020-11-25 06:19:15 +0700

    feat(Naxx/Gluth): add enrage timer and correct other timers

--- DBM-Naxx/ConstructQuarter/Gluth.lua
+++ DBM-Naxx/ConstructQuarter/Gluth.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Gluth", "DBM-Naxx", 2)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 2869 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 3000 $"):sub(12, -3))
 mod:SetCreatureID(15932)
 
 mod:RegisterCombat("combat")
@@ -9,20 +9,24 @@ mod:RegisterCombat("combat")
 mod:EnableModel()
 
 mod:RegisterEvents(
-	"SPELL_DAMAGE"
+	"SPELL_DAMAGE",
+	"SPELL_AURA_APPLIED"
 )
-
 
 local warnDecimateSoon	= mod:NewSoonAnnounce(54426, 2)
 local warnDecimateNow	= mod:NewSpellAnnounce(54426, 3)
+local warnEnrageSoon	= mod:NewSoonAnnounce(28371, 2)
 
-local enrageTimer		= mod:NewBerserkTimer(420)
-local timerDecimate		= mod:NewCDTimer(104, 54426)
+local berserkTimer		= mod:NewBerserkTimer(480)
+local timerDecimate		= mod:NewCDTimer(105, 54426)
+local timerEnrage		= mod:NewCDTimer(30, 28371)
 
 function mod:OnCombatStart(delay)
-	enrageTimer:Start(420 - delay)
-	timerDecimate:Start(110 - delay)
-	warnDecimateSoon:Schedule(100 - delay)
+	berserkTimer:Start(480 - delay)
+	timerDecimate:Start(105 - delay)
+	warnDecimateSoon:Schedule(95 - delay)
+	timerEnrage:Start(30 - delay)
+	warnEnrageSoon:Schedule(25 - delay)
 end
 
 local decimateSpam = 0
@@ -31,8 +35,13 @@ function mod:SPELL_DAMAGE(args)
 		decimateSpam = GetTime()
 		warnDecimateNow:Show()
 		timerDecimate:Start()
-		warnDecimateSoon:Schedule(96)
+		warnDecimateSoon:Schedule(95)
 	end
 end
 
-
+function mod:SPELL_AURA_APPLIED(args)
+	if (args:IsSpellID(28371, 54427)) then
+		timerEnrage:Start()
+		warnEnrageSoon:Schedule(25)
+	end
+end
