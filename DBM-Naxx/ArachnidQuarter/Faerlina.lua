2642c399e9eac583cbed26307ccb7ca5003edc60
Winfidonarleyan <dowlandtop@yandex.com>
2020-11-25 08:41:53 +0700

    feat(Naxx/Faerlina): correct timers

--- DBM-Naxx/ArachnidQuarter/Faerlina.lua
+++ DBM-Naxx/ArachnidQuarter/Faerlina.lua
@@ -14,23 +14,21 @@ mod:RegisterEvents(
 local warnEmbraceActive		= mod:NewSpellAnnounce(28732, 1)
 local warnEmbraceExpire		= mod:NewAnnounce("WarningEmbraceExpire", 2, 28732)
 local warnEmbraceExpired	= mod:NewAnnounce("WarningEmbraceExpired", 3, 28732)
-local warnEnrageSoon		= mod:NewSoonAnnounce(28131, 3)
-local warnEnrageNow			= mod:NewSpellAnnounce(28131, 4)
+local warnEnrageSoon		= mod:NewSoonAnnounce(28798, 3)
+local warnEnrageNow			= mod:NewSpellAnnounce(28798, 4)
 
 local timerEmbrace			= mod:NewBuffActiveTimer(30, 28732)
-local timerEnrage			= mod:NewCDTimer(60, 28131)
+local timerEnrage			= mod:NewCDTimer(60, 28798)
 
 local embraceSpam = 0
 local enraged = false
 
 function mod:OnCombatStart(delay)
-	timerEnrage:Start(-delay)
-	warnEnrageSoon:Schedule(55 - delay)
 	enraged = false
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(28732, 54097)				-- Widow's Embrace
+	if args:IsSpellID(28732, 54097)			-- Widow's Embrace
 	and (GetTime() - embraceSpam) > 5 then  -- This spell is casted twice in Naxx 25 (bug?)
 		embraceSpam = GetTime()
 		warnEmbraceExpire:Cancel()
@@ -38,8 +36,8 @@ function mod:SPELL_CAST_SUCCESS(args)
 		warnEnrageSoon:Cancel()
 		timerEnrage:Stop()
 		if enraged then
-			timerEnrage:Start()
-			warnEnrageSoon:Schedule(45)
+			timerEnrage:Start(60)
+			warnEnrageSoon:Schedule(55)
 		end
 		timerEmbrace:Start()
 		warnEmbraceActive:Show()
@@ -50,9 +48,18 @@ function mod:SPELL_CAST_SUCCESS(args)
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpellID(28798, 54100) then			-- Frenzy
+	if args:IsSpellID(28798, 54100) then -- Frenzy
 		warnEnrageNow:Show()
-		enraged = GetTime()
+		
+		if enraged then
+			timerEnrage:Start(30)
+			warnEnrageSoon:Schedule(25)
+		else
+			timerEnrage:Start(60)
+			warnEnrageSoon:Schedule(55)
+		end
+		
+		enraged = true
 	end
 end
 
