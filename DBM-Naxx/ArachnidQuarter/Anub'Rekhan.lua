c7ff3084d75879bb70c4627690641b6f757240ed
Winfidonarleyan <dowlandtop@yandex.com>
2020-11-25 12:13:49 +0700

    feat(Naxx): set revision 3000 for Anub'Rekhan and Faerlina

--- DBM-Naxx/ArachnidQuarter/Anub'Rekhan.lua
+++ DBM-Naxx/ArachnidQuarter/Anub'Rekhan.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Anub'Rekhan", "DBM-Naxx", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 2943 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 3000 $"):sub(12, -3))
 mod:SetCreatureID(15956)
 
 mod:RegisterCombat("combat")
