2628b93b079b9b61185b90a18509d0994892027a
Winfidonarleyan <dowlandtop@yandex.com>
2020-11-26 17:24:51 +0700

    feat(Naxx/Noth): add blink timer and correct teleport timer

--- DBM-Naxx/PlagueQuarter/Noth.lua
+++ DBM-Naxx/PlagueQuarter/Noth.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Noth", "DBM-Naxx", 3)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision(("$Revision: 2248 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 3000 $"):sub(12, -3))
 mod:SetCreatureID(15954)
 
 mod:RegisterCombat("combat")
@@ -13,45 +13,70 @@ mod:RegisterEvents(
 local warnTeleportNow	= mod:NewAnnounce("WarningTeleportNow", 3, 46573)
 local warnTeleportSoon	= mod:NewAnnounce("WarningTeleportSoon", 1, 46573)
 local warnCurse			= mod:NewSpellAnnounce(29213, 2)
+local warnBlink			= mod:NewSoonAnnounce(29208, 2)
 
-local timerTeleport		= mod:NewTimer(90, "TimerTeleport", 46573)
+local timerTeleport		= mod:NewTimer(110, "TimerTeleport", 46573)
 local timerTeleportBack	= mod:NewTimer(70, "TimerTeleportBack", 46573)
+local timerBlink		= mod:NewCDTimer(30, 29208)
 
-local phase = 0
+local firstBlink = true
+local blickCastCount = 0
 
 function mod:OnCombatStart(delay)
-	phase = 0
-	self:BackInRoom(delay)
+	self:InRoom()
+	blickCastCount = 0
+	firstBlink = true
 end
 
 function mod:Balcony()
-	local timer
-	if phase == 1 then timer = 70
-	elseif phase == 2 then timer = 97
-	elseif phase == 3 then timer = 120
-	else return	end
+	local timer = 70
+	blickCastCount = 0
+	firstBlink = true
+	
 	timerTeleportBack:Show(timer)
-	warnTeleportSoon:Schedule(timer - 20)
+	warnTeleportSoon:Schedule(timer - 10)
 	warnTeleportNow:Schedule(timer)
-	self:ScheduleMethod(timer, "BackInRoom")
+	self:ScheduleMethod(timer, "InRoom")
+	self:UnscheduleMethod("CastBlink")
 end
 
-function mod:BackInRoom(delay)
-	delay = delay or 0
-	phase = phase + 1
-	local timer
-	if phase == 1 then timer = 90 - delay
-	elseif phase == 2 then timer = 110 - delay
-	elseif phase == 3 then timer = 180 - delay
-	else return end
+function mod:InRoom()
+	local timer = 110
+	
 	timerTeleport:Show(timer)
-	warnTeleportSoon:Schedule(timer - 20)
+	warnTeleportSoon:Schedule(timer - 10)
 	warnTeleportNow:Schedule(timer)
 	self:ScheduleMethod(timer, "Balcony")
+
+	if mod:IsDifficulty("normal25") or mod:IsDifficulty("heroic25") then
+		if (firstBlink == true) then
+			self:CastBlink(26)
+		else
+			self:CastBlink(30)
+		end
+	end
+end
+
+function mod:CastBlink(delay)
+	if (delay == 26) then 
+		firstBlink = false 
+	end
+
+	local timer = delay or 30
+	blickCastCount = blickCastCount + 1
+
+	timerBlink:Start(timer)
+	warnBlink:Schedule(timer - 5)
+	
+	if (blickCastCount >= 2) then
+		return
+	end
+	
+	self:ScheduleMethod(timer, "CastBlink")
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(29213, 54835) then	-- Curse of the Plaguebringer
+	if args:IsSpellID(29213, 54835) then -- Curse of the Plaguebringer
 		warnCurse:Show()
 	end
 end
